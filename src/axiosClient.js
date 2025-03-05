5e507b53b33f98b8228922c83aebd200858e44a5
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-05 18:58:51 +0800

    axiosClient.js: Added interceptor to attach open ai api token

--- src/axiosClient.js
+++ src/axiosClient.js
@@ -8,4 +8,15 @@ const axiosClient = axios.create(
   }
 )
 
+// Request Interceptor - Attach Open AI Token
+axiosClient.interceptors.request.use(
+  async ( config ) => {
+    if ( process.env.REACT_APP_OPENAI_API_KEY ) {
+      config.headers.Authorization = `Bearer ${process.env.REACT_APP_OPENAI_API_KEY}`
+    }
+    return config
+  },
+  ( error ) => Promise.reject(error)
+)
+
 export default axiosClient
