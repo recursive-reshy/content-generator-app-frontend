acf6d6c31632b807ccaaf76091d42feb606eef23
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-06 13:50:35 +0800

    PageMyStyles.jsx: Added PageMyStyles

---
+++ src/pages/PageMyStyles.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+// Componets
+import UploadFile from '../components/UploadFile'
+
+const PageMyStyles = () => {
+  return (
+    <UploadFile />
+  )
+}
+
+export default PageMyStyles
