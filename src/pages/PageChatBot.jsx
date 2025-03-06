ea00921c92c14ad3ddb870305e19918841c13e1a
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-06 18:35:16 +0800

    pages\PageChatBot.jsx: Added container and grid to handle layout of children

--- src/pages/PageChatBot.jsx
+++ src/pages/PageChatBot.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
+// MUI Core
+import Grid from '@mui/material/Grid2'
+
 // Components
 import ChatInterface from '../components/ChatInterface'
 
 const PageChatBot = () => {
   return (
-    <ChatInterface />
+    <Grid 
+      container
+      sx={ { height: '100%' } }
+    >
+      <Grid item size={12}>
+        <ChatInterface />
+      </Grid>
+    </Grid>
   )
 }
 
