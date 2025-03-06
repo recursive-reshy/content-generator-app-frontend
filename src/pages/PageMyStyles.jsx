e54f24a595c3b16c448dca5b2a420c4b3909be02
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-06 18:35:41 +0800

    pages\PageMyStyles.jsx: Added container and grid to handle layout of children

--- src/pages/PageMyStyles.jsx
+++ src/pages/PageMyStyles.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 
+// MUI Core
+import Grid from '@mui/material/Grid2'
+
 // Componets
 import UploadFile from '../components/UploadFile'
 
 const PageMyStyles = () => {
   return (
-    <UploadFile />
+    <Grid 
+      container
+      sx={ { height: '100%' } }
+    >
+      <Grid item size={12}>
+        <UploadFile />
+      </Grid>
+    </Grid>
   )
 }
 
