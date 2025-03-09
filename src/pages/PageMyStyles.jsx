58dc8744f1587fa420cebdf82c8df15de8930fde
recursive-reshy <61969021+recursive-reshy@users.noreply.github.com>
2025-03-09 18:20:25 +0800

    pages\PageMyStyles.jsx: Integrated file upload api

--- src/pages/PageMyStyles.jsx
+++ src/pages/PageMyStyles.jsx
@@ -1,10 +1,12 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 
 // MUI Core
 import Grid from '@mui/material/Grid2'
+import LinearProgress from '@mui/material/LinearProgress'
 
 // Componets
 import UploadFile from '../components/UploadFile'
+import { Notifications } from '../components'
 
 // Services
 import { uploadFiles } from '../apis/fileService.js'
@@ -14,6 +16,42 @@ const PageMyStyles = () => {
   const [ uploadStatus, setUploadStatus ] = useState('')
   const [ isUploading, setIsUploading ] = useState(false)
 
+  // Value from input onChange
+  const [ uploadFilesData, setUploadFilesData ] = useState()
+
+  const [ isNotificationOpen, setIsNotificationOpen ] = useState(false)
+  
+  useEffect( () => {
+    if( uploadFilesData ) {
+      handleUploadFiles( uploadFilesData )
+    }
+
+    if( uploadStatus ) {
+      setIsNotificationOpen(true)
+    }
+  }, [ uploadStatus, uploadFilesData ] )
+
+  // TODO: I believe this can be simplified. We can remove the state uploadFilesData and just set e.target.value to '' in the handleChange
+  const handleUploadFiles = async () => {
+    try {
+
+      const formData = new FormData()
+
+      formData.append( 'files', uploadFilesData )
+
+      const { data: { message } } = await uploadFiles( formData )
+      
+      setUploadStatus(message)
+      setIsUploading(false)
+      setUploadFilesData()
+    } catch (error) {
+      console.error(`Error uploading files: ${error}`)
+      setUploadStatus('Error uploading files')
+      setIsUploading(false)
+      setUploadFilesData()
+    }
+  }
+
   return (
     <Grid 
       container
@@ -21,30 +59,30 @@ const PageMyStyles = () => {
     >
       <Grid item size={12}>
         <UploadFile
-          handleChange={ async event => {
+          loading={isUploading}
+          disabled={isUploading}
+          handleChange={ event => {
+            setUploadStatus()
             if ( !event.target.files && !event.target.files.length ) {
               setUploadStatus('No files selected')
               return
             }
 
             setIsUploading(true)
-        
-            const formData = new FormData()
 
-            formData.append( 'files', event.target.files[0] )
-        
-            try {
-              const respomse = await uploadFiles( formData )
-              setUploadStatus('Files uploaded successfully!')
-              setIsUploading(false)
-            } catch (error) {
-              console.error(`Error uploading files: ${error}`)
-              setUploadStatus('Error uploading files')
-              setIsUploading(false)
-            }
+            setUploadFilesData( event.target.files[0] )
+
+            // Clear the event target values so that files of the same name can be uploaded again
+            event.target.value = ''
           } }
         />
       </Grid>
+      <Notifications
+        message={uploadStatus}
+        open={isNotificationOpen}
+        setOpen={setIsNotificationOpen}
+        severity={ uploadStatus == 'File uploaded successfully' ? 'success' : 'error' }
+      />
     </Grid>
   )
 }
